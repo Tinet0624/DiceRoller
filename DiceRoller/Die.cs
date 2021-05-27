ea6935f888b51286fb361e2a4d65fffb1c91365f
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-05-27 11:50:12 -0700

    Add project files.

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    /// <summary>
+    /// Represents a single standard 6-sided die
+    /// </summary>
+    class Die
+    {
+        // one random object for all instances of die = static
+        private static Random rand;
+        /// <summary>
+        /// Runs before any other instances are created
+        /// </summary>
+        
+        static Die()
+        {
+            rand = new Random();
+        }
+
+        /// <summary>
+        /// Rolls die after die is constructed to begin with
+        /// a random number.
+        /// </summary>
+        public Die()
+        {
+            
+            Roll();
+        }
+
+        /// <summary>
+        /// The currently rolled value.
+        /// </summary>
+        public byte FaceUpValue { get; private set; }
+
+        /// <summary>
+        /// Roles die and give the face up value a randome number
+        /// between 1 and 6 inclusive.
+        /// </summary>
+        /// <Returns>returns the rolled number</Returns>>
+        public byte Roll()
+        {
+            const int minDieInBound = 1;
+            const int maxDieExUpperBound = 7;
+
+            //random needs to be fixed. Will not work in succession.
+            // make random object to share
+            //Random rand = new Random();
+            int numRolled = rand.Next(minDieInBound, maxDieExUpperBound);
+
+            FaceUpValue = (byte)numRolled;
+            return FaceUpValue;
+        }
+    }
+}
---
+++ DiceRoller/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceRoller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Die myDie = new Die();
+            MessageBox.Show($"You rolled {myDie.Roll()}");
+            myDie.Roll();
+            MessageBox.Show($"You rolled {myDie.FaceUpValue}");
+        }
+    }
+}
